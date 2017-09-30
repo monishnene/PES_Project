b5cdc2cbf2863155bced0976b0633bd6831fc665
sanikadongre <sado9543@colorado.edu>
2017-09-30 17:54:08 -0600

    Add files via upload

---
+++ debug.c
@@ -0,0 +1,15 @@
+/*
+ * debug.c
+ *
+ *  Created on: Sep 29, 2017
+ *      Author: monish
+ */
+
+void print_memory(uint8_t * start, uint32_t length)
+{
+	short i;
+	for (i=0;i<length;i++)
+	{
+		printf("%x",*(start+i));
+	}
+}
---
+++ debug.h
@@ -0,0 +1,13 @@
+/*
+ * debug.h
+ *
+ *  Created on: Sep 29, 2017
+ *      Author: monis
+ */
+
+#ifndef DEBUG_H_
+#define DEBUG_H_
+
+void print_memory(uint8_t * start, uint32_t length);
+
+#endif /* DEBUG_H_ */
