4a186169a302a5036c1b1a4ec9a2f04f5502841d
Monish.Nene <monish.nene@colorado.edu>
2017-10-29 16:38:19 -0600

    circbuf uart updated

---
+++ src/project2.c
@@ -0,0 +1,14 @@
+#include <stdint.h>
+
+#include <stdlib.h>
+#include "circbuf.h"
+#include "uart.h"
+#include "conversion.h"
+
+void project2(void)
+{
+while(1)
+{
+/*conditionforrx_buffer full)*/
+void UART0_IRQHandler(void);
+}
