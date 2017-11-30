a7f87abe254612a2d752a702342e7368893ea77d
sanikadongre <sado9543@colorado.edu>
2017-11-30 14:33:33 -0700

    SPI stubs

--- src/spi.c
+++ src/spi.c
@@ -2,41 +2,44 @@
 #include "SPI.h"
 
 void SPI_init(void) {
-	
-	SIM->SCGC5 |= SIM_SCGC5_PORTC_MASK;
-	SIM->SCGC4 |= SIM_SCGC4_SPI0_MASK;
-	PTC_BASE_PTR->PDDR |= 0x10; 
-	PTC_BASE_PTR->PDOR |= 0x10; 
-	PORTC->PCR[4] = PORT_PCR_MUX(1);  
-	PORTC->PCR[5] = PORT_PCR_MUX(2);  
-	PORTC->PCR[6] = PORT_PCR_MUX(2); 
-	PORTC->PCR[7] = PORT_PCR_MUX(2); 
+	// Enable clock network to SPI0
+	SIM->SCGC5 |= SIM_SCGC5_PORTC_MASK;// Clock get to PORT C
+	SIM->SCGC4 |= SIM_SCGC4_SPI0_MASK;//Clock get to SPI0
+	PTC_BASE_PTR->PDDR |= 0x10; //Set pin 4 port C i.e PCS as output direction
+	PTC_BASE_PTR->PDOR |= 0x10; //Set pin 4 port C i.e PCS as output
 
-	PTC_BASE_PTR->PSOR = 1<<4;
-	SPI0->C1 = 0x50;
-	SPI0_BR = (SPI_BR_SPPR(0x03) | SPI_BR_SPR(0x08));  
+	// configure output crossbar
+	PORTC->PCR[4] = PORT_PCR_MUX(1);  // PCS
+	PORTC->PCR[5] = PORT_PCR_MUX(2);  // SCK
+	PORTC->PCR[6] = PORT_PCR_MUX(2);  // MOSI
+	PORTC->PCR[7] = PORT_PCR_MUX(2);  // MISO
 
+	PTC_BASE_PTR->PSOR = 1<<4;//make gpio chip high
+	// configure gpio address select
+	/* here */
 
-void SPI_read_byte(uint8_t byte)
-{
-
-   uint8_t i=0;
-   while ((SPI_status() & 0x10) == 0x10)
-   i = SPI0->D;
-   return 0;
+	// Config registers, turn on SPI0 as master
+	// Enable chip select
+	SPI0->C1 = 0x50;// Enable SPI and set as Master
+	SPI0_BR = (SPI_BR_SPPR(0x03) | SPI_BR_SPR(0x08));     //Set baud rate prescale divisor to 3 & set baud rate divisor to 512 for baud rate of 15625 hz
 }
-                   
 
 uint8_t SPI_write_byte(uint8_t byte) 
 
 {
                 uint8_t x;
-                while ((SPI_status() & 0x20) != 0x20); 
-		SPI0->D = x;
-		while ((SPI_status() & 0x80) != 0x80); 
-		return SPI0->D; 
+                while ((SPI_status() & 0x20) != 0x20); //Check if transfer buffer is empty
+		SPI0->D = x;//Send data
+		while ((SPI_status() & 0x80) != 0x80); //Check if transfer is complete, hence recieve buffer full
+		return SPI0->D; //return data
 }
 
+
+void spi_flush() //To flush out the transmit and recieve buffers
+{
+	SPI0_C1&=0xBF;
+	SPI_init();
+}
 
 void SPI_send_packet(uint8_t* p, size_t length)
 {
@@ -50,10 +53,12 @@ void SPI_send_packet(uint8_t* p, size_t length)
             return 0;
 }
 
-void spi_flush() 
+void SPI_read_byte(uint8_t byte)
 {
-	SPI0_C1&=0xBF;
-	SPI_init();
+
+   uint8_t i=0;
+   while ((SPI_status() & 0x10) == 0x10)
+   i = SPI0->D;
+   return 0;
 }
-
-
+                
