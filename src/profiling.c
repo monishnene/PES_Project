99ec87805093a4beeba2bf396063a5f5ac8a2b63
monishnene <31666297+monishnene@users.noreply.github.com>
2017-12-02 16:45:51 -0700

    Update profiling.c

--- src/profiling.c
+++ src/profiling.c
@@ -1,53 +1,58 @@
-#include "memory.c"
+/*
+ * profiling.c
+ *
+ *  Created on: Nov 30, 2017
+ *      Author: monish
+ */
 #include "memory.h"
-#include <stdlib.h>
 #include <stdint.h>
+#include "System_MKL25Z4.h"
+#include "MKL25Z4.h"
+#include <stdlib.h>
+#include <string.h>
 
-void size( uint16_t length, uint8_t case)
+
+uint32_t time_calculation(/*parameter for function*/)
 {
-  length = 10;
-  uint8_t* dst = uint8_t* malloc(length);
-  uint8_t* src = uint8_t* malloc(length);
-  uint8_t* i;
-  uint8_t* j;
-  uint8_t value = 5;
-  i = my_memset = (src, length, value);
-  
-  switch (case)
-   
-  {
-    case 1: 
-    memset(src,value,length);
-    memmove(dst,src,length);
-
-    case 2: 
-    
-    src = my_memset(src, length, value);
-    dst = my_memmove( src, dst, length);
-    
-    case 3:
- 
-     src = my_memset(src, length, value);
-     dst = my_memmove( src, dst, length);
-    
-
-    case 4: 
-    memset_dma(uint8_t *dst, uint32_t length);
-    DMA_DAR0 = dma_st memmove_dma(uint8_t *src, uint8_t *dst, uint32_t length);
-
+	uint32_t pre_interrupt=0,post_interrupt=0,profiling_time;
+	pre_interrupt = SysTick-> LOAD;
+	//memory transfer functions
+	post_interrupt = SysTick-> LOAD;
+	profiling_time = pre_interrupt - post_interrupt;
+	return profiling_time;
 }
- 
-size(uint16_t 100, uint8_t case)
-size(uint16_t 1000, uint8_t case)
-size(uint16_t 5000, uint8_t case)
 
-free_words(dst);
-free_words(src);
-return 0;
+void profiling( uint16_t length, uint8_t memory_function)
+{
+	uint8_t* dst = (uint8_t*) malloc(length);
+	uint8_t* src = (uint8_t*) malloc(length);
+	uint32_t value = 5;
+	switch (memory_function)
+	{
+		case 1:
+		src=memset((void*)src,(uint8_t)value,4*length);
+		src=memmove((void*)dst,(void*)src,4*length);
+		break;
+
+		case 2:
+		src = my_memset(src, length, (uint8_t)value);
+		dst = my_memmove( src, dst, length);
+		break;
+
+		case 3:
+		src = my_memset(src, length, (uint8_t) value);
+		dst = my_memmove( src, dst, length);
+		break;
+
+		case 4:
+		memset_dma(src,length,value);
+		memmove_dma(src,dst,length);
+		break;
+	}
+	free_words((uint32_t*)dst);
+	free_words((uint32_t*)src);
+	return;
 }
-  
-
-
 
 
 
