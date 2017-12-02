25c399953e7b9f2f67cc2d847138eb5e8f80bed8
sanikadongre <sado9543@colorado.edu>
2017-12-02 11:59:01 -0700

    adding memory

--- include/common/memory.h
+++ include/common/memory.h
@@ -9,15 +9,15 @@
 #define MEMORY_H_
 #include <stdint.h>
 
-uint8_t* my_memmove(unsigned char* src, unsigned char* dst, int length);
-unsigned char* my_memcpy(unsigned char* src, unsigned char* dst, int length);
-unsigned char* my_memset(unsigned char* src, int length, unsigned char value);
-unsigned char* my_memzero(unsigned char* src, int length);
-uint8_t* my_reverse(unsigned char* src, int length);
-int* reserve_words(int length);
+uint8_t* my_memmove(uint8_t* src, uint8_t* dst, size_t length);
+uint8_t* my_memcpy(uint8_t* src, uint8_t* dst, size_t length);
+uint8_t* my_memset(uint8_t* src, size_t length, uint8_t value);
+uint8_t* my_memzero(uint8_t* src, size_t length);
+uint8_t* my_reverse(uint8_t* src, size_t length);
+int32_t* reserve_words(size_t length);
 void free_words(uint32_t* src);
 void memmove_dma(uint8_t *src, uint8_t *dst, uint32_t length);
-void memset_dma(uint8_t *dst, uint32_t length);
+void memset_dma(uint8_t *dst, uint32_t length, uint32_t value);
 
 
 #endif /* MEMORY_H_ */
