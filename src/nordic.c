c02b686e01d3da2ac90d0f465a71b5ee7c0a01bb
sanikadongre <sado9543@colorado.edu>
2017-12-02 16:36:11 -0700

    nordic

--- src/nordic.c
+++ src/nordic.c
@@ -3,6 +3,13 @@
 #include "NRF.h"
 #include "SPI.h"
 
+/***********************************************************************
+ * @brief nrf_read_register
+ * It reads the SPI_write register in a variable and returns the value
+ 
+
+ ***********************************************************************/
+
 uint8_t nrf_read_register(uint8_t register)
 
 {
@@ -11,11 +18,27 @@ uint8_t nrf_read_register(uint8_t register)
         return l;
 }
 
+/***********************************************************************
+ * @brief nrf_write_register
+ * It sets the SPI_write register to a value
+ 
+
+ ***********************************************************************/
+
 void nrf_write_register(uint8_t register, uint8_t value)
 {
  value = 255;
  SPI_write(value|register); //write command
 }
+
+/***********************************************************************
+ * @brief nrf_read_status
+ * It is a function to read the nordic status register
+ * SPI_write has been set to FF and a variable is used to store the result
+ * for SPI_read
+ 
+
+ ***********************************************************************/
 
 void nrf_read_status() //function to read the nrf status register
 {
@@ -27,23 +50,39 @@ void nrf_read_status() //function to read the nrf status register
 	PTC_BASE_PTR->PSOR = 1<<4;
 }
 
+/***********************************************************************
+ * @brief nrf_write_config
+ * Chip select is low
+ * It is a function to write command to the Config register
+ * Data is written to config register and that data is being stored in 
+ * config variable. The chip select is then set to high
+ ***********************************************************************/
+
 void nrf_write_config(uint8_t config)
 {
 	PTC_BASE_PTR->PCOR = 1<<4; //Chip select low
-	config = nrf_write_register(NORDIC_CONFIG_REG); //write command to Config register
-	SPI_write(0x03);//Write data to Config register
+        nrf_write_register(NORDIC_CONFIG_REG); //write command to Config register
+	config = SPI_write(0x03);//Write data to Config register
 	PTC_BASE_PTR->PSOR =  1<<4; //Chip select high
 }
 
+/***********************************************************************
+ * @brief nrf_read_config
+ * The Clock to Port B is being enabled.
+ * Chip select is low
+ * It is a function to read command to the Config register
+ * A dummy value is sent to receive data  
+ * The chip select is then set to high and the dummy value is being returned
+ ***********************************************************************/
 uint8_t nrf_read_config()
 {
 	SIM_SCGC5 |= SIM_SCGC5_PORTB_MASK; //enable clock to port B
 	uint8_t j,k;
 	PTC_BASE_PTR->PCOR = 1<<4; //Chip select low
-	k = nrf_read_register(NORDIC_CONFIG_REG); // read command to Config register
+        nrf_read_register(NORDIC_CONFIG_REG); // read command to Config register
 	j=SPI_write(0X7F);//Send dummy value to recieve data
 	PTC_BASE_PTR->PSOR = 1<<4; //Chip select high
-        return k;
+        return j;
 }
 
 void nrf_read_rf_setup() //read the rf register
@@ -68,7 +107,7 @@ uint8_t nrf_read_rf_ch();
 {
  uint8_t i,k;
  PTC_BASE_PTR->PCOR = 1<<4;
- nrf_write_register(NORDIC_RF_CH);
+ nrf_write_register(NORDIC_RF_CH_REG);
  i = SPI_write(0xff);
  PTC_BASE_PTR->PSOR = 1<<4;
  k=nrf_read_fifo_status();
@@ -79,7 +118,7 @@ void nrf_write_rf_ch(uint8_t channel);
 {
 uint32_t i;
 PTC_BASE_PTR->PCOR = 1<<4;
-channel = nrf_write_register(NORDIC_RF_CH);
+channel = nrf_write_register(NORDIC_RF_CH_REG);
 for(i=0; i<5; i++)
 {
   SPI_write(0x10);
