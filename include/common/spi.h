182252d87a910e813e47bb424050fc88f8216123
sanikadongre <sado9543@colorado.edu>
2017-12-03 11:13:10 -0700

    Add files via upload

--- include/common/spi.h
+++ include/common/spi.h
@@ -12,9 +12,10 @@
 #include <stdint.h>
 
 void SPI_init();
-void SPI_read_byte( uint8_t byte);
+void SPI_read_byte( uint8_t* byte);
 void SPI_write_byte( uint8_t byte);
-void SPI_send_packet( uint8_t* p, size_t length);
+void SPI_send_packet( uint8_t* p, uint8_t length);
 void SPI_flush();
+uint8_t SPI_state(void);
 
 #endif
