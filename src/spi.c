681f1de3b2604647b97e8b1437afd1597923b233
monishnene <31666297+monishnene@users.noreply.github.com>
2017-12-03 10:43:08 -0700

    Update spi.c

--- src/spi.c
+++ src/spi.c
@@ -1,3 +1,9 @@
+/*
+ * spi.c
+ *
+ *  Created on: Dec 3, 2017
+ *      Author: monis
+ */
 #include "MKL25Z4.h"
 #include "SPI.h"
 
@@ -15,7 +21,7 @@ void SPI_init(void) {
 	PTC_BASE_PTR->PDDR |= 0x10; //Set pin 4 port C i.e PCS as output direction
 	PTC_BASE_PTR->PDOR |= 0x10; //Set pin 4 port C i.e PCS as output
 	PORTC->PCR[4] = PORT_PCR_MUX(1);  // PCS
-	PORTC->PCR[5] = PORT_PCR_MUX(2);  
+	PORTC->PCR[5] = PORT_PCR_MUX(2);
 	PORTC->PCR[6] = PORT_PCR_MUX(2);  // MOSI
 	PORTC->PCR[7] = PORT_PCR_MUX(2);  // MISO
 
@@ -24,20 +30,18 @@ void SPI_init(void) {
 	SPI0_BR = (SPI_BR_SPPR(0x03) | SPI_BR_SPR(0x08));     //Set  the baud rate prescale divisor to 3 & set baud rate divisor to 512 for baud rate of 15625 hz
 }
 /***********************************************************************
- * @brief SPI_write_byte  
+ * @brief SPI_write_byte
  * It checks if the transmit buffer is empty and sends the data
- * It then checks if the transfer is complete i.e. receiver buffer is full 
+ * It then checks if the transfer is complete i.e. receiver buffer is full
  *  and then returns the data
  ***********************************************************************/
 
-uint8_t SPI_write_byte(uint8_t byte) 
+void SPI_write_byte(uint8_t byte)
 
 {
-                uint8_t byte;
-                while ((SPI_status() & 0x20) != 0x20); //Checks if the transfer buffer is empty
-		SPI0->D = byte;//Send  the data
+		SPI0->D = byte; //Send  the data
 		while ((SPI_status() & 0x80) != 0x80); //if receive buffer is full then the transfer is complete
-		return SPI0->D; //return the  data
+		return; //return the  data
 }
 
 /***********************************************************************
@@ -54,11 +58,11 @@ void spi_flush() //To flush out the  spi transmit and recieve buffers
 /***********************************************************************
  * @brief SPI_send_packet
  * It checks if the transmit buffer is empty and sends the packet
- * It then checks if the transfer is complete i.e. receiver buffer is full 
+ * It then checks if the transfer is complete i.e. receiver buffer is full
 
  ***********************************************************************/
 
-void SPI_send_packet(uint8_t* p, size_t length) //To send the SPI packet
+void SPI_send_packet(uint8_t* p, uint8_t length) //To send the SPI packet
 {
              uint8_t i=0;
              while((SPI_status() & 0x20) != 0x20); //Checks if the transfer buffer is empty
@@ -67,22 +71,20 @@ void SPI_send_packet(uint8_t* p, size_t length) //To send the SPI packet
               SPI0->D = *(p+i); //Send the packet
           }
             while((SPI_status() & 0x80) != 0x80); //Check if the transfer is complete if the receive buffer is full
-            return 0;
+            return;
 }
 
 /***********************************************************************
  * @brief SPI_read_byte
- * It checks if the transfer is complete i.e. receiver buffer is full 
+ * It checks if the transfer is complete i.e. receiver buffer is full
  * SPI byte is read
 
  ***********************************************************************/
 
-void SPI_read_byte(uint8_t byte)
+void SPI_read_byte(uint8_t* byte)
 {
-
-   uint8_t byte=0;
    while ((SPI_status() & 0x80) != 0x80); //If the receive buffer is full then the transfer is complete
-   byte = SPI0->D; //Read the data 
-   return 0; 
+   *(byte) = SPI0->D; //Read the data
 }
-                
+
+
