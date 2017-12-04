cd1158a38066980d78057da13563a59abcc9d0b9
monishnene <31666297+monishnene@users.noreply.github.com>
2017-12-04 10:23:19 -0700

    Create binary_logger.c

---
+++ src/binary_logger.c
@@ -0,0 +1,271 @@
+/*
+ * binary_logger.c
+ *
+ *  Created on: Nov 25, 2017
+ *      Author: monish
+ */
+
+#include "circbuf.h"
+#include "uart.h"
+#include "binary_logger.h"
+#include "conversion.h"
+#include <stdint.h>
+#include "System_MKL25Z4.h"
+#include "MKL25Z4.h"
+#define log_size 100
+
+void log_buffer_configure(void)
+{
+	uint8_t i;
+	i = CB_init(&log_buffer,log_size);
+	log_buffer.head = buffer.buffptr;
+	log_buffer.tail = buffer.buffptr;
+	log_buffer.count = 0;
+	log_buffer.length = log_size;
+	return;
+}
+
+void log_item(uint8_t log_id_current)
+{
+	uint32_t real_time=1162167621;
+	uint32_t* real_time_ptr= &real_time;
+	uint32_t count=0,i=0,j=0,count_char=0;
+	uint8_t temp=0;
+	uint8_t* temp_ptr = &temp;
+	switch(log_id_current)
+	{
+		case LOGGER_INITIALIZED:
+		{
+			count+=log_integer(log_id_current);
+			count+=log_data(real_time_ptr,1);
+			count+=log_integer(0);
+			count+=log_integer(count);
+			break;
+		}
+
+		case GPIO_INITIALIZED:
+		{
+			count+=log_integer(log_id_current);
+			count+=log_data(real_time_ptr,1);
+			count+=log_integer(0);
+			count+=log_integer(count);
+			break;
+		}
+
+		case SYSTEM_INITIALIZED:
+		{
+			count+=log_integer(log_id_current);
+			count+=log_data(real_time_ptr,1);
+			count+=log_integer(0);
+			count+=log_integer(count);
+			break;
+		}
+
+		case SYSTEM_HALTED:
+		{
+			count+=log_integer(log_id_current);
+			count+=log_data(real_time_ptr,1);
+			count+=log_integer(0);
+			count+=log_integer(count);
+			break;
+		}
+
+		case INFO:
+		{
+			count+=log_integer(log_id_current);
+			count+=log_data(real_time_ptr,1);
+			count+=log_integer(log_buffer.count);
+			for(i=0;i<log_buffer.count;i++)
+			{
+				j = CB_buffer_remove_item(&log_buffer,temp_ptr);
+				count += log_string(temp_ptr,1);
+			}
+			count+=log_integer(count);
+			break;
+		}
+
+		case WARNING:
+		{
+			count+=log_integer(log_id_current);
+			count+=log_data(real_time_ptr,1);
+			count+=log_integer(log_buffer.count);
+			for(i=0;i<log_buffer.count;i++)
+			{
+				j = CB_buffer_remove_item(&log_buffer,temp_ptr);
+				count += log_string(temp_ptr,1);
+			}
+			count+=log_integer(count);
+			break;
+		}
+
+		case ERROR:
+		{
+			count+=log_integer(log_id_current);
+			count+=log_data(real_time_ptr,1);
+			count+=log_integer(log_buffer.count);
+			for(i=0;i<log_buffer.count;i++)
+			{
+				j = CB_buffer_remove_item(&log_buffer,temp_ptr);
+				count += log_string(temp_ptr,1);
+			}
+			count+=log_integer(count);
+			break;
+		}
+
+		case PROFILING_STARTED:
+		{
+			count+=log_integer(log_id_current);
+			count+=log_data(real_time_ptr,1);
+			count+=log_integer(0);
+			count+=log_integer(count);
+			break;
+		}
+
+		case PROFILING_RESULT:
+		{
+			count+=log_integer(log_id_current);
+			count+=log_data(real_time_ptr,1);
+			count+=log_integer(log_buffer.count);
+			for(i=0;i<log_buffer.count;i++)
+			{
+				j = CB_buffer_remove_item(&log_buffer,temp_ptr);
+				count += log_string(temp_ptr,1);
+			}
+			count+=log_integer(count);
+			break;
+		}
+
+		case DATA_RECEIVED:
+		{
+			count+=log_integer(log_id_current);
+			count+=log_data(real_time_ptr,1);
+			count+=log_integer(0);
+			count+=log_integer(count);
+			break;
+		}
+
+		case PROFILING_COMPLETED:
+		{
+			count+=log_integer(log_id_current);
+			count+=log_data(real_time_ptr,1);
+			count+=log_integer(0);
+			count+=log_integer(count);
+			break;
+		}
+
+		case DATA_ANALYSIS_STARTED:
+		{
+			count+=log_integer(log_id_current);
+			count+=log_data(real_time_ptr,1);
+			count+=log_integer(0);
+			count+=log_integer(count);
+			break;
+		}
+
+		case DATA_ALPHA_COUNT:
+		{
+			count+=log_integer(log_id_current);
+			count+=log_data(real_time_ptr,1);
+			count_char = get_alpha_count();
+			count+=log_integer(1);
+			count+=log_integer(count_char);
+			count+=log_integer(count);
+			break;
+		}
+
+		case DATA_NUMERIC_COUNT:
+		{
+			count+=log_integer(log_id_current);
+			count+=log_data(real_time_ptr,1);
+			count_char = get_nums_count();
+			count+=log_integer(1);
+			count+=log_integer(count_char);
+			count+=log_integer(count);
+			break;
+		}
+
+		case DATA_PUNCTUATION_COUNT:
+		{
+			count+=log_integer(log_id_current);
+			count+=log_data(real_time_ptr,1);
+			count_char = get_punc_count();
+			count+=log_integer(1);
+			count+=log_integer(count_char);
+			count+=log_integer(count);
+			break;
+		}
+
+		case DATA_MISC_COUNT:
+		{
+			count+=log_integer(log_id_current);
+			count+=log_data(real_time_ptr,1);
+			count_char = get_misc_count();
+			count+=log_integer(1);
+			count+=log_integer(count_char);
+			count+=log_integer(count);
+			break;
+		}
+
+		case DATA_ANALYSIS_COMPLETED:
+		{
+			count+=log_integer(log_id_current);
+			count+=log_data(real_time_ptr,1);
+			count+=log_integer(0);
+			count+=log_integer(count);
+			break;
+		}
+
+		case HEARTBEAT:
+		{
+			count+=log_integer(log_id_current);
+			count+=log_data(real_time_ptr,1);
+			count+=log_integer(count);
+			break;
+		}
+	}
+}
+
+
+uint32_t log_data(uint32_t* src, uint32_t length)
+{
+	uint8_t* src2=(uint8_t*) src;
+	uint8_t i=0,k=0,count=0;
+	for(i=0;i<4*length;i++)
+	{
+		CB_buffer_add_item(&buffer,*src2);
+		count += get_count_of_1(*src2);
+	}
+	return count;
+}
+
+uint32_t log_string(uint8_t* src, uint32_t length)
+{
+
+	uint8_t i=0,k=0,count=0;
+	for(i=0;i<=length;i++)
+	{
+		CB_buffer_add_item(&buffer,*src);
+		count += get_count_of_1(*src);
+	}
+	return count;
+}
+
+uint32_t log_integer(uint32_t src)
+{
+	uint8_t i=0,j=0,k=0,count=0;
+	uint8_t array[20];
+	uint8_t* value = array;
+	j = my_itoa(src,value,10);
+	for(i=0;i<j;i++)
+	{
+		CB_buffer_add_item(&buffer,*(value+i));
+		count += get_count_of_1(*(value+i));
+	}
+	return count;
+}
+
+uint8_t log_flush(void)
+{
+	uint8_t i=CB_is_empty(&buffer);
+	return i;
+}
