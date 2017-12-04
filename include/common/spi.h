84acc250dc72562fc487db1dd135dcba0365ff0b
sanikadongre <sado9543@colorado.edu>
2017-12-03 21:32:53 -0700

    Add files via upload

--- include/common/spi.h
+++ include/common/spi.h
@@ -1,19 +1,10 @@
-/*
-* spi.h
-*
-*
-* Created on: Nov 19, 2017
-*
-* Author: Sanika
-*/
-
 #ifndef SPI_H_
 #define SPI_H_
 #include <stdint.h>
 
 void SPI_init();
-void SPI_read_byte( uint8_t* byte);
-void SPI_write_byte( uint8_t byte);
+uint8_t SPI_read_byte( uint8_t byte);
+uint8_t SPI_write_byte( uint8_t byte);
 void SPI_send_packet( uint8_t* p, uint8_t length);
 void SPI_flush();
 uint8_t SPI_state(void);
