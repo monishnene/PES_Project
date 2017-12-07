721377e9604aeeaff52ce5131a11f55f0a382035
monishnene <31666297+monishnene@users.noreply.github.com>
2017-12-07 00:32:25 -0700

    Update profiling.h

--- include/common/profiling.h
+++ include/common/profiling.h
@@ -7,8 +7,8 @@
 
 #ifndef INCLUDES_PROFILING_H_
 #define INCLUDES_PROFILING_H_
-
+static uint8_t loop_for_big_memory=1;
+void SysTick_Init();
 uint32_t time_calculation(/*parameter for function*/);
-void profiling( uint16_t length, uint8_t memory_function);
-
+void profiling( uint32_t length);
 #endif /* INCLUDES_PROFILING_H_ */
