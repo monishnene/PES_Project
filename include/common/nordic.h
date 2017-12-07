f25d8119e44cc97e4b6b0f06c778cab9d1393ccb
sanikadongre <sado9543@colorado.edu>
2017-12-07 11:47:12 -0700

    Update nordic.h

--- include/common/nordic.h
+++ include/common/nordic.h
@@ -32,17 +32,18 @@
 #define W_TXPAYLOAD (0xA0)
 #define R_RXPAYLOAD (0x61)
 
-uint8_t nrf_read_register(uint8_t address);
-void nrf_write_register(uint8_t address, uint8_t value);
-uint8_t nrf_read_status();
-uint8_t nrf_write_config(uint8_t config);
-uint8_t nrf_read_config();
-uint8_t nrf_read_rf_setup();
-uint8_t nrf_read_rf_ch();
-void nrf_write_rf_ch(uint8_t channel);
-void nrf_read_TX_ADDR(uint8_t* address);
-void nrf_write_TX_ADDR(uint8_t* tx_addr);
-uint8_t nrf_read_fifo_status();
+void  nrf_read_register(uint8_t register_function);
+void nrf_write_register(uint8_t register_function);
+void nrf_read_status();
+void nrf_write_config();
+void nrf_read_config();
+void nrf_read_rf_setup();
+void nrf_write_rf_setup()
+void nrf_read_rf_ch();
+void nrf_write_rf_ch();
+void nrf_read_TX_ADDR();
+void nrf_write_TX_ADDR();
+void nrf_read_fifo_status();
 void nrf_flush_tx_fifo();
 void nrf_flush_rx_fifo();
 #endif
