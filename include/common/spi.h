c865624a78aca625e1c119979bd406b30810bb76
sanikadongre <sado9543@colorado.edu>
2017-12-07 21:49:34 -0700

    Update spi.h

--- include/common/spi.h
+++ include/common/spi.h
@@ -1,12 +1,10 @@
-/*
-* spi.h
-*
-*
-* Created on: Nov 19, 2017
-*
-* Author: Sanika
-*/
 
+/***************************
+*
+* spi.h
+* Author : Sanika
+*
+ ***************************/
 #ifndef SPI_H_
 #define SPI_H_
 #include <stdint.h>
@@ -15,9 +13,8 @@ void SPI_init();
 uint8_t SPI_state();
 uint8_t SPI_write_byte( uint8_t byte);
 void SPI_send_packet( uint8_t* p, uint8_t length);
-uint8_t SPI_read_byte(uint8_t byte);
+uint8_t SPI_read_byte();
 void SPI_flush();
 
 
 #endif
-
