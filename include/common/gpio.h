dc3c91bae019bf12e0f657a1932cb1ad7d913ba5
sanikadongre <sado9543@colorado.edu>
2017-12-08 14:30:03 -0700

    Update gpio.h

--- include/common/gpio.h
+++ include/common/gpio.h
@@ -1,11 +1,15 @@
-#include <stdio.h>
-#include <stdint.h>
-#include "spi.h"
+#include "MKL25Z4.h"
+#include "SPI.h"
 #include "nordic.h"
 
 #ifndef GPIO_H_
 #define GPIO_H_
 
-void GPIO_nrf_init();
+
+void SPI_configure();
+void GPIO_nrf_configure();
+void nrf_chip_enable();
+void nrf_chip_disable();
+void nrf_transmit_enable();
 
 #endif
