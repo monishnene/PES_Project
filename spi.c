f0275266d5ad4edab079c2483f42cba684aba6c1
sanikadongre <sado9543@colorado.edu>
2017-12-08 19:21:47 -0700

    documented spi

--- spi.c
+++ spi.c
@@ -1,88 +1,82 @@
+/*
+ * spi.c
+ * Created on : Nov 22, 2017
+ * Author : Sanika
+ */
+
 #include "MKL25Z4.h"
 #include "SPI.h"
+#include "gpio.h"
 
-
-/***********************************************************************
- Function that initializes the SPI
-
+/************************************************************************
+ * @brief SPI_init()
+ * Function to initialize the SPI
+ * SPI_configure() is being called to configure the required GPIO pins
+ * The SPI is then enabled and set as the Master
+ * The baud rate prescale and baud rate divisor is then being set
  ***********************************************************************/
 
-
-void SPI_init(void) {
-	// Enable clock network to SPI0
-	SIM->SCGC5 |= SIM_SCGC5_PORTC_MASK;// Clock for PORT C
-	SIM->SCGC4 |= SIM_SCGC4_SPI0_MASK;//Clock for SPI0
-	PTC_BASE_PTR->PDDR |= 0x10; //Set pin 4 port C i.e PCS as output direction
-	PTC_BASE_PTR->PDOR |= 0x10; //Set pin 4 port C i.e PCS as output
-	PORTC->PCR[4] = PORT_PCR_MUX(1);  // PCS
-	PORTC->PCR[5] = PORT_PCR_MUX(2);  
-	PORTC->PCR[6] = PORT_PCR_MUX(2);  // MOSI
-	PORTC->PCR[7] = PORT_PCR_MUX(2);  // MISO
-
-	PTC_BASE_PTR->PSOR = 1<<4;//make gpio chip high
-	SPI0->C1 = 0x50;// Enable SPI as master
-	SPI0_BR = (SPI_BR_SPPR(0x03) | SPI_BR_SPR(0x08));     //Set  the baud rate prescale divisor to 3 & set baud rate divisor to 512 for baud rate of 15625 hz
+void SPI_init() {
+	SPI_configure();
+	SPI0->C1 = 0x50;// Enables the SPI and is being set as  the Master device
+	SPI0_BR = (SPI_BR_SPPR(0x03) | SPI_BR_SPR(0x08));    //Baud rate prescale divisor and baud rate divisor is being set
 }
-/***********************************************************************
- * @brief SPI_write_byte  
- * It checks if the transmit buffer is empty and sends the data
- * It then checks if the transfer is complete i.e. receiver buffer is full 
- *  and then returns the data
+/************************************************************************
+ * @brief SPI_state()
+ * Function that shows the state of SPI
  ***********************************************************************/
 
-uint8_t SPI_write_byte(uint8_t byte) 
-
+uint8_t SPI_state(void) {  //Shows the status of SPI
+	return SPI0->S;
+}
+/************************************************************************
+ * @brief SPI_write_byte(uint8_t byte)
+ * Function to write byte to the SPI
+ * It writes the byte if the transmitter buffer is empty
+ ***********************************************************************/
+void SPI_write_byte(uint8_t byte)
 {
-                uint8_t byte;
-                while ((SPI_status() & 0x20) != 0x20); //Checks if the transfer buffer is empty
-		SPI0->D = byte;//Send  the data
-		while ((SPI_status() & 0x80) != 0x80); //if receive buffer is full then the transfer is complete
-		return SPI0->D; //return the  data
+        while ((SPI_state() & 0x20) != 0x20); //To check if the Transmitter buffer is empty
+		SPI0->D = byte;                       //Sends the data
+
 }
 
-/***********************************************************************
- * @brief SPI_flush()
- * Function that flushes out the SPI transmit and the recieve buffers
-
+/************************************************************************
+ * @brief SPI_send_packet(uint8_t* p, uint8_t length)
+ * Function to send packet
+ * To check if the transmit buffer is empty and then sends the data for given
+ * length
  ***********************************************************************/
-void spi_flush() //To flush out the  spi transmit and recieve buffers
+void SPI_send_packet(uint8_t* p, uint8_t length) {
+	uint8_t i=0;
+	for (i = 0; i < length; i++) {
+		while ((SPI_state() & 0x20) != 0x20);
+		SPI0->D = *(p+i);                        //Sends packet
+	}
+}
+/************************************************************************
+ * @brief SPI_read_byte()
+ * Function to read the SPI
+ * SPI_configure() is being called to configure the required GPIO pins
+ * The SPI is then enabled and set as the Master
+ * The baud rate prescale and baud rate divisor is then being set
+ ***********************************************************************/
+
+uint8_t SPI_read_byte()
+{
+	uint8_t byte;
+	SPI0->D = 0x00;                             //Test value pushed to SPI
+	while ((SPI_state() & 0x80) != 0x80);       //To check if the receive buffer is empty
+	byte = SPI0->D;                             // Reads byte
+	return byte;                                // Returns the byte
+}
+/************************************************************************
+ * @brief SPI_flush()
+ * Function to flush the transmit and receive buffers
+ ***********************************************************************/
+
+void spi_flush() //To flush out the transmit and recieve buffers
 {
 	SPI0_C1&=0xBF;
 	SPI_init();
 }
-
-/***********************************************************************
- * @brief SPI_send_packet
- * It checks if the transmit buffer is empty and sends the packet
- * It then checks if the transfer is complete i.e. receiver buffer is full 
-
- ***********************************************************************/
-
-void SPI_send_packet(uint8_t* p, size_t length) //To send the SPI packet
-{
-             uint8_t i=0;
-             while((SPI_status() & 0x20) != 0x20); //Checks if the transfer buffer is empty
-             for(i=0;i<length;i++)
-          {
-              SPI0->D = *(p+i); //Send the packet
-          }
-            while((SPI_status() & 0x80) != 0x80); //Check if the transfer is complete if the receive buffer is full
-            return 0;
-}
-
-/***********************************************************************
- * @brief SPI_read_byte
- * It checks if the transfer is complete i.e. receiver buffer is full 
- * SPI byte is read
-
- ***********************************************************************/
-
-void SPI_read_byte(uint8_t byte)
-{
-
-   uint8_t byte=0;
-   while ((SPI_status() & 0x80) != 0x80); //If the receive buffer is full then the transfer is complete
-   byte = SPI0->D; //Read the data 
-   return 0; 
-}
-                
