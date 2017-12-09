6361c056e4cc42b6c381ef20ede4d26575a17102
monishnene <31666297+monishnene@users.noreply.github.com>
2017-12-08 22:08:56 -0700

    Update spi.h

--- include/common/spi.h
+++ include/common/spi.h
@@ -6,7 +6,7 @@ void SPI_init();
 uint8_t SPI_state();
 void SPI_write_byte( uint8_t byte);
 void SPI_send_packet( uint8_t* p, uint8_t length);
-uint8_t SPI_read_byte();
+uint8_t SPI_read_byte(uint8_t byte);
 void SPI_flush();
 
 
