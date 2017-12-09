f00cf22b941461c09870e8c776c03914657ccc01
sanikadongre <sado9543@colorado.edu>
2017-12-09 13:41:15 -0700

    spi.h modified

--- include/common/spi.h
+++ include/common/spi.h
@@ -4,9 +4,9 @@
 
 void SPI_init();
 uint8_t SPI_state();
-void SPI_write_byte( uint8_t byte);
+void SPI_write_byte(uint8_t byte);
 void SPI_send_packet( uint8_t* p, uint8_t length);
-uint8_t SPI_read_byte(uint8_t byte);
+uint8_t SPI_read_byte();
 void SPI_flush();
 
 
