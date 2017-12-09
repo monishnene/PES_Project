b21e9296d2f86361f68c44a9d9ea0321b441b6e3
sanikadongre <sado9543@colorado.edu>
2017-12-09 13:39:22 -0700

    gpio.h modified

--- include/common/gpio.h
+++ include/common/gpio.h
@@ -1,21 +1,12 @@
-/*
- * gpio.h
- *
- *  Created on: Dec 8, 2017
- *      Author: Monish and Sanika
- */
-
-#ifndef INCLUDES_GPIO_H_
-#define INCLUDES_GPIO_H_
 #include "MKL25Z4.h"
 #include "SPI.h"
 #include "nordic.h"
 
-void SPI_configure();
-void GPIO_nrf_configure();
-void nrf_chip_enable();
-void nrf_chip_disable();
-void nrf_transmit_enable();
-void nrf_transmit_disable();
+#ifndef INCLUDES_GPIO_H_
+#define INCLUDES_GPIO_H_
 
-#endif /* INCLUDES_GPIO_H_ */
+void GPIO_nrf_configure();
+
+#endif
+
+
