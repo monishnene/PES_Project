8fb1b54cafd6be4690f0902a3fe549c7e13ab02e
sanikadongre <sado9543@colorado.edu>
2017-12-09 13:36:36 -0700

    spi.c update

--- src/spi.c
+++ src/spi.c
@@ -6,7 +6,7 @@
 
 #include "MKL25Z4.h"
 #include "SPI.h"
-#include "gpio.h"
+
 
 /************************************************************************
  * @brief SPI_init()
@@ -17,9 +17,25 @@
  ***********************************************************************/
 
 void SPI_init() {
-	SPI_configure();
-	SPI0->C1 = 0x50;// Enables the SPI and is being set as  the Master device
-	SPI0_BR = (SPI_BR_SPPR(0x03) | SPI_BR_SPR(0x08));    //Baud rate prescale divisor and baud rate divisor is being set
+	SIM->SCGC5 |= SIM_SCGC5_PORTC_MASK;// Clock get to PORT C
+		SIM->SCGC4 |= SIM_SCGC4_SPI0_MASK;//Clock get to SPI0
+		PTC_BASE_PTR->PDDR |= 0x10; //Set pin 4 port C i.e PCS as output direction
+		PTC_BASE_PTR->PDOR |= 0x10; //Set pin 4 port C i.e PCS as output
+
+		// configure output crossbar
+		PORTC->PCR[4] = PORT_PCR_MUX(1);  // PCS
+		PORTC->PCR[5] = PORT_PCR_MUX(2);  // SCK
+		PORTC->PCR[6] = PORT_PCR_MUX(2);  // MOSI
+		PORTC->PCR[7] = PORT_PCR_MUX(2);  // MISO
+
+		PTC_BASE_PTR->PSOR = 1<<4;//make gpio chip high
+		// configure gpio address select
+		/* here */
+
+		// Config registers, turn on SPI0 as master
+		// Enable chip select
+		SPI0->C1 = 0x50;// Enable SPI and set as Master
+		SPI0_BR = (SPI_BR_SPPR(0x03) | SPI_BR_SPR(0x08)); //Baud rate prescale divisor and baud rate divisor is being set
 }
 /************************************************************************
  * @brief SPI_state()
@@ -38,6 +54,7 @@ void SPI_write_byte(uint8_t byte)
 {
         while ((SPI_state() & 0x20) != 0x20); //To check if the Transmitter buffer is empty
 		SPI0->D = byte;                       //Sends the data
+
 }
 
 /************************************************************************
@@ -61,9 +78,10 @@ void SPI_send_packet(uint8_t* p, uint8_t length) {
  * The baud rate prescale and baud rate divisor is then being set
  ***********************************************************************/
 
-uint8_t SPI_read_byte(uint8_t byte)
+uint8_t SPI_read_byte()
 {
-	SPI0->D = byte;                             //Test value pushed to SPI
+	uint8_t byte;
+	SPI0->D = 0x00;                             //Test value pushed to SPI
 	while ((SPI_state() & 0x80) != 0x80);       //To check if the receive buffer is empty
 	byte = SPI0->D;                             // Reads byte
 	return byte;                                // Returns the byte
