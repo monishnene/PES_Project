df51429adee4f94cf14d74dddd85bbf1827ceb20
sanikadongre <sado9543@colorado.edu>
2017-12-10 02:57:20 -0700

    final nordic

--- include/common/nordic.h
+++ include/common/nordic.h
@@ -4,7 +4,7 @@
 *
 * Created on: Nov 19, 2017
 *
-* Author: Sanika
+* Author: Sanika and monish
 */
 
 #ifndef NORDIC_H_
@@ -13,8 +13,8 @@
 #include <stdint.h>
 #define nrf_chip_enable() GPIOC->PCOR = 1<<4;
 #define nrf_chip_disable() GPIOC->PSOR =  1<<4;
-#define nrf_transmit_enable() GPIOC->PCOR = 1<<3;
-#define nrf_transmit_disable() GPIO->PSOR = 1<<3;
+#define nrf_transmit_enable() GPIOC->PCOR = 1<<2;
+#define nrf_transmit_disable() GPIO->PSOR = 1<<2;
 
 #define NRF_CONFIG_REG (0x00)
 #define NRF_TXADDR_REG (0x10)
@@ -25,20 +25,20 @@
 #define RX_ADDR_P1_REG (0x0B)
 #define TX_ADDR (0x10)
 #define FIFO_STATUS_REG (0x17)
-//#define NRF_POWER_UP (0x01)
-//#define NRF_POWER_UP_MASK (0x02)
-//#define NRF_POWER_DOWN_MASK (0x00)
-//#define NRF_EN_AA_REG (0x01)
-//#define NRF_EN_RXADDR_REG (0x02)
-//#define NRF_SETUP_AW_REG (0x03)
-//#define NRF_SETUP_RETR_REG (0x04)
-//#define NRF_PIPESIZE (0X11)
-//#define NRF_ENRXADDR (0x02)
-//#define W_TXPAYLOAD (0xA0)
-//#define R_RXPAYLOAD (0x61)
+#define NRF_POWER_UP (0x01)
+#define NRF_POWER_UP_MASK (0x02)
+#define NRF_POWER_DOWN_MASK (0x00)
+#define NRF_EN_AA_REG (0x01)
+#define NRF_EN_RXADDR_REG (0x02)
+#define NRF_SETUP_AW_REG (0x03)
+#define NRF_SETUP_RETR_REG (0x04)
+#define NRF_PIPESIZE (0X11)
+#define NRF_ENRXADDR (0x02)
+#define W_TXPAYLOAD (0xA0)
+#define R_RXPAYLOAD (0x61)
 
-uint8_t nrf_read_register(void);
-void nrf_write_register(uint8_t value);
+void nrf_read_register(uint8_t function);
+void nrf_write_register(uint8_t function);
 void nrf_read_status();
 void nrf_write_config();
 void nrf_read_config();
